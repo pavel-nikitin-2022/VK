4ae60157df209884c88966392bc786cd90cc0567
pavel-nikitin-2022 <90633463+pavel-nikitin-2022@users.noreply.github.com>
2022-04-08 13:33:18 +0300

    modal completed

--- src/modals/gifs.js
+++ src/modals/gifs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {
     List,
     ModalPage,
@@ -9,6 +9,7 @@ import "@vkontakte/vkui/dist/vkui.css";
 
 
 export default function Gifs({ list, setActiveModal }) {
+    const [array, change] = useState(list)
     return (
         <ModalPage
             id="photo"
@@ -21,7 +22,7 @@ export default function Gifs({ list, setActiveModal }) {
         >
 
             <Group>
-                <List>
+                {/*<List style={{height: "400px", overflow: "scroll"}}>
                     {list.map((data, i) => {
                         return (
                             <div key={i} style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
@@ -31,7 +32,47 @@ export default function Gifs({ list, setActiveModal }) {
                             </div>
                         );
                     })}
-                </List>
+                </List>*/}
+                <table style={{ width: "100%" }}>
+                    <tbody style={{ width: "100%" }}>
+                        {/*<tr style={{width: "100%"}}>
+                        <td>
+                            <div style={{height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                <img width={window.innerWidth / 3} src={list[0][0].url} />
+                            </div>
+                        </td>
+                        <td>
+                            <div style={{height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                <img width={window.innerWidth / 3} src={list[1][0].url} />
+                            </div>
+                        </td>
+                        <td>
+                            <div style={{height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                <img width={window.innerWidth / 3} src={list[2][0].url} />
+                            </div>
+                        </td>
+                       </tr>*/}
+                        {list.map((data, i) => {
+                            if (i % 3 == 0) {
+                                return (
+                                    <tr key={i} style={{ width: "100%" }}>
+                                        {list.map((data, k) => {
+                                            if (k >= i && k < i + 3) {
+                                                return (
+                                                    <td key={k}>
+                                                        <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black"}}>
+                                                            <img width={window.innerWidth / 3} src={array[k][0].url} />
+                                                        </div>
+                                                    </td>
+                                                );
+                                            }
+                                        })}
+                                    </tr>
+                                );
+                            }
+                        })}
+                    </tbody>
+                </table>
             </Group>
         </ModalPage>
     );
