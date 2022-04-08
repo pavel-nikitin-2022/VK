d8293fb78140ac8fba0981fa712cdf37935c404f
pavel-nikitin-2022 <90633463+pavel-nikitin-2022@users.noreply.github.com>
2022-04-08 15:35:21 +0300

    madal total complete

--- src/modals/gifs.js
+++ src/modals/gifs.js
@@ -1,11 +1,11 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import {
-    List,
     ModalPage,
     ModalPageHeader,
     Group,
 } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
+import socket from '../socket';
 
 
 export default function Gifs({ list, setActiveModal }) {
@@ -22,37 +22,9 @@ export default function Gifs({ list, setActiveModal }) {
         >
 
             <Group>
-                {/*<List style={{height: "400px", overflow: "scroll"}}>
-                    {list.map((data, i) => {
-                        return (
-                            <div key={i} style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
-                                <div style={{ width: "150px", height: "150px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                                    <img src={data[0].url} />
-                                </div>
-                            </div>
-                        );
-                    })}
-                </List>*/}
                 <table style={{ width: "100%" }}>
                     <tbody style={{ width: "100%" }}>
-                        {/*<tr style={{width: "100%"}}>
-                        <td>
-                            <div style={{height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                                <img width={window.innerWidth / 3} src={list[0][0].url} />
-                            </div>
-                        </td>
-                        <td>
-                            <div style={{height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                                <img width={window.innerWidth / 3} src={list[1][0].url} />
-                            </div>
-                        </td>
-                        <td>
-                            <div style={{height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                                <img width={window.innerWidth / 3} src={list[2][0].url} />
-                            </div>
-                        </td>
-                       </tr>*/}
-                        {list.map((data, i) => {
+                        {/*list.map((data, i) => {
                             if (i % 3 == 0) {
                                 return (
                                     <tr key={i} style={{ width: "100%" }}>
@@ -60,13 +32,48 @@ export default function Gifs({ list, setActiveModal }) {
                                             if (k >= i && k < i + 3) {
                                                 return (
                                                     <td key={k}>
-                                                        <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black"}}>
-                                                            <img width={window.innerWidth / 3} src={array[k][0].url} />
+                                                        <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black" }}>
+                                                            {window.innerWidth < 420 && <img onClick={()=>{socket.emit("send", [array[k][0].url, "g"]); setActiveModal(null)}} width={window.innerWidth / 3} src={array[k][0].url} />}
+                                                            {window.innerWidth > 420 && <img onClick={()=>{socket.emit("send", [array[k][0].url, "g"]); setActiveModal(null)}} width={100} src={array[k][0].url} />}
                                                         </div>
                                                     </td>
                                                 );
                                             }
                                         })}
+                                    </tr>
+                                );
+                            }
+                        })*/}
+                        {list.map((data, i) => {
+                            if (i % 3 == 0) {
+                                return (
+                                    <tr key={i} style={{ width: "100%" }}>
+                                        <td>
+                                            <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black" }}>
+                                                {window.innerWidth < 420 && <img onClick={() => { socket.emit("send", [array[i][0].url, "g"]); setActiveModal(null) }} width={window.innerWidth / 3} src={array[i][0].url} />}
+                                                {window.innerWidth > 420 && <img onClick={() => { socket.emit("send", [array[i][0].url, "g"]); setActiveModal(null) }} width={135} src={array[i][0].url} />}
+                                            </div>
+                                        </td>
+                                        <td>
+                                            {array[i + 1] && <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black" }}>
+                                                {window.innerWidth < 420 && <img onClick={() => { socket.emit("send", [array[i + 1][0].url, "g"]); setActiveModal(null) }} width={window.innerWidth / 3} src={array[i + 1][0].url} />}
+                                                {window.innerWidth > 420 && <img onClick={() => { socket.emit("send", [array[i + 1][0].url, "g"]); setActiveModal(null) }} width={135} src={array[i + 1][0].url} />}
+                                            </div>}
+                                            {!array[i + 1] && <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black",opacity: 0 }}>
+                                                {window.innerWidth < 420 && <img width={window.innerWidth / 3} src={array[i][0].url} />}
+                                                {window.innerWidth > 420 && <img width={135} src={array[i][0].url} />}
+                                            </div>}
+                                        </td>
+                                        <td>
+                                            {array[i + 2] && <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black" }}>
+                                                {window.innerWidth < 420 && <img onClick={() => { socket.emit("send", [array[i + 2][0].url, "g"]); setActiveModal(null) }} width={window.innerWidth / 3} src={array[i + 2][0].url} />}
+                                                {window.innerWidth > 420 && <img onClick={() => { socket.emit("send", [array[i + 2][0].url, "g"]); setActiveModal(null) }} width={135} src={array[i + 2][0].url} />}
+                                            </div>}
+                                            {!array[i + 2] && <div style={{ height: "100px", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black",opacity: 0 }}>
+                                                {window.innerWidth < 420 && <img width={window.innerWidth / 3} src={array[i][0].url} />}
+                                                {window.innerWidth > 420 && <img width={135} src={array[i][0].url} />}
+                                            </div>}
+                                        </td>
                                     </tr>
                                 );
                             }
